9d581c15048a8f7f4ae9d4a2b9db9f1f799a0781
Sedanur Çevik <sdnrcvk@gmail.com>
2023-03-03 22:07:26 +0300

    routing created

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Blog from './pages/blog/Blog';
+import Create from './pages/create/Create';
+import Search from './pages/search/Search';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/'>
+            <Home/>
+          </Route>
+          <Route path='/create'>
+            <Create/>
+          </Route>
+          <Route path='/search'>
+            <Search/>
+          </Route>
+          <Route path='/blog/:id'>
+            <Blog/>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/blog/Blog.js
@@ -0,0 +1,11 @@
+import './Blog.css'
+
+import React from 'react'
+
+export default function Blog() {
+  return (
+    <div>
+        <h1>Blog</h1>
+    </div>
+  )
+}
