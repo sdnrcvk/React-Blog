16b164e68766de1916341853840d09807321bdef
Sedanur Çevik <sdnrcvk@gmail.com>
2023-03-04 20:08:57 +0300

    Blog page edited and styled

--- src/pages/blog/Blog.js
+++ src/pages/blog/Blog.js
@@ -1,11 +1,29 @@
+import {useParams} from 'react-router-dom';
+import {useFetch} from '../../hooks/useFetch';
+
 import './Blog.css'
 
-import React from 'react'
-
 export default function Blog() {
-  return (
-    <div>
-        <h1>Blog</h1>
-    </div>
-  )
+
+    const {id}=useParams()
+    const url='http://localhost:8000/bloglar/'+id;
+
+    const {hata,yukleniyor,data:blog}=useFetch(url);
+
+    return (
+        <div className="blog">
+            {hata && <p className="error">{hata}</p>}
+            {yukleniyor && <p className="loading">{yukleniyor}</p>}
+            {blog && (
+              <>
+                <h2 className="page-title">{blog.baslik}</h2>
+                <p className="time">{blog.okunmaSuresi} okuma süresi</p>
+                <ul>
+                  {blog.kategoriler.map(kat => <li key={kat}>{kat}</li>)}
+                </ul>
+                <p className="info">{blog.icerik}</p>
+              </>
+            )}           
+        </div>
+    )
 }
