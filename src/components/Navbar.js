d8579633007e0569e5648bd30f290cc92f96a4fc
Sedanur Çevik <sdnrcvk@gmail.com>
2023-03-05 01:49:48 +0300

    Searchbar component created

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import './Navbar.css'
 import React from 'react'
+import Searchbar from "./Searchbar";
 
 export default function Navbar() {
   return (
@@ -9,6 +10,7 @@ export default function Navbar() {
             <Link to="/" className="brand">
                 <h1>SC Blog</h1>
             </Link>
+            <Searchbar/>
             <Link to="/create">
                 Yeni Yazı
             </Link>
