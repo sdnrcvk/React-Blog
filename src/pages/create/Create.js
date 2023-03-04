568a2b01b4d2707cb8ebe8838cbf810ae5b834cb
Sedanur Çevik <sdnrcvk@gmail.com>
2023-03-05 01:02:16 +0300

    create page edited and styled

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,11 +1,55 @@
+import { useState } from 'react'
 import './Create.css'
 
-import React from 'react'
+export default function Create() {  
+  const [baslik, setBaslik] = useState('')
+  const [icerik, setIcerik] = useState('')
+  const [okunmaSuresi, setOkunmaSuresi] = useState('')
+  
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(baslik, icerik, okunmaSuresi)
+  }
 
-export default function Create() {
   return (
-    <div>
-        <h1>Create</h1>
+    <div className="create">
+      <h2 className="page-title">Yeni Yazı</h2>
+      <form onSubmit={handleSubmit}>
+
+        <label>
+          <span>Yazı Başlık:</span>
+          <input 
+            type="text" 
+            onChange={(e) => setBaslik(e.target.value)}
+            value={baslik}
+            required
+          />
+        </label>
+
+        {/* recipe ingredients here */}
+
+        <label>
+          <span>Yazı İçerik:</span>
+          <textarea 
+            onChange={(e) => setIcerik(e.target.value)}
+            value={icerik}
+      rows={5}
+            required
+          />
+        </label>
+
+        <label>
+          <span>Okunma Süresi:</span>
+          <input 
+            type="number" 
+            onChange={(e) => setOkunmaSuresi(e.target.value)}
+            value={okunmaSuresi}
+            required 
+          />
+        </label>
+
+        <button className="btn">Oluştur</button>
+      </form>
     </div>
   )
 }
