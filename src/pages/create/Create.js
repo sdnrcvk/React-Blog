c4a3ed18849da532e672e374fb30506877069eaf
Sedanur Çevik <sdnrcvk@gmail.com>
2023-03-05 01:15:04 +0300

    Create page edited for categories

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,20 +1,49 @@
-import { useState } from 'react'
+import { useState,useRef  } from 'react'
 import './Create.css'
 
 export default function Create() {  
   const [baslik, setBaslik] = useState('')
   const [icerik, setIcerik] = useState('')
   const [okunmaSuresi, setOkunmaSuresi] = useState('')
-  
+  const [yeniKategori, setYeniKategori] = useState('')
+  const [kategoriler, setKategoriler] = useState([])
+  const kategoriInput = useRef(null)
+
   const handleSubmit = (e) => {
     e.preventDefault()
     console.log(baslik, icerik, okunmaSuresi)
+    console.log(...kategoriler)
   }
+
+  const handleAdd = (e) => {
+    e.preventDefault()
+    const yKat = yeniKategori.trim()
+
+    if (yKat && !kategoriler.includes(yKat)) {
+      setKategoriler(oKat => [...oKat, yeniKategori])
+    }
+    setYeniKategori('')
+    kategoriInput.current.focus()
+  }
+
 
   return (
     <div className="create">
       <h2 className="page-title">Yeni Yazı</h2>
       <form onSubmit={handleSubmit}>
+        <label>
+          <span>Yazı Kategorileri:</span>
+          <div className="categories">
+          <input 
+            type="text" 
+            onChange={(e) => setYeniKategori(e.target.value)}
+            value={yeniKategori}
+            ref={kategoriInput}
+            />
+            <button onClick={handleAdd} className="btnAdd btn">ekle</button>
+          </div>
+        </label>
+        <p>Kategoriler:<span className="list"> {kategoriler.map(i => <em key={i}>{i}, </em>)}</span></p>
 
         <label>
           <span>Yazı Başlık:</span>
