eb7e19d8d714e5f525aeea2565d3b54ffcdcc178
Sedanur Çevik <sdnrcvk@gmail.com>
2023-03-13 16:58:41 +0300

    accessed to context data

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,10 +2,13 @@ import { Link } from "react-router-dom";
 import './Navbar.css'
 import React from 'react'
 import Searchbar from "./Searchbar";
+import { useTheme } from "../hooks/useTheme";
 
 export default function Navbar() {
+  const {bgColor}=useTheme();
+
   return (
-    <div className="navbar">
+    <div className="navbar" style={{background:bgColor}}>
         <nav>
             <Link to="/" className="brand">
                 <h1>SC Blog</h1>
--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -5,7 +5,7 @@ export const ThemeContext = createContext()
 export function ThemeProvider({ children }) {
 
   return (
-    <ThemeContext.Provider>
+    <ThemeContext.Provider value={{bgColor:"#778beb"}}>
       {children}
     </ThemeContext.Provider>
   )
