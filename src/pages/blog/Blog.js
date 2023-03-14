c28cb15246a77026985edfc3bdb1cfaa65bc8a94
Sedanur Çevik <sdnrcvk@gmail.com>
2023-03-14 16:33:36 +0300

    accessed to firebase data by id

--- src/pages/blog/Blog.js
+++ src/pages/blog/Blog.js
@@ -1,16 +1,40 @@
 import {useParams} from 'react-router-dom';
 import {useFetch} from '../../hooks/useFetch';
 import { useTheme } from '../../hooks/useTheme';
-
+import { useState, useEffect } from 'react';
+import { db } from '../../firebase/config';
+import { doc,getDoc } from 'firebase/firestore';
 import './Blog.css'
 
 export default function Blog() {
 
     const {id}=useParams()
-    const url='http://localhost:8000/bloglar/'+id;
+    //const url='http://localhost:8000/bloglar/'+id;
     const {mode}=useTheme();
 
-    const {hata,yukleniyor,data:blog}=useFetch(url);
+    //const {hata,yukleniyor,data:blog}=useFetch(url);
+
+    const [blog,setBlog]=useState();
+    const [yukleniyor, setYukleniyor] = useState(false)
+    const [hata, setHata] = useState(false)
+
+    useEffect(()=>{
+      setYukleniyor(true);
+
+      const ref=doc(db,"bloglar",id);
+
+      getDoc(ref).then(doc=>{
+        //console.log(doc.data());
+        if (doc.exists){
+          setYukleniyor(false);
+          setBlog(doc.data())
+        }else{
+          setYukleniyor(false);
+          setHata("Veriye erişilemedi")
+        }
+      })
+
+    },[id])
 
     return (
         <div className={`blog ${mode}`}>
