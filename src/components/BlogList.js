344a33068ca0e472991bf746ea0ba706bd654844
Sedanur Çevik <sdnrcvk@gmail.com>
2023-03-14 16:56:49 +0300

    data deleted from firebase

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,9 +1,21 @@
 import { Link } from 'react-router-dom'
 import { useTheme } from '../hooks/useTheme'
 import './BlogList.css'
+import DeleteIcon from "../assets/deleteIcon.svg"
+import {db} from "../firebase/config"
+import {  deleteDoc, doc } from 'firebase/firestore' 
 
 export default function BlogList({bloglar}) {
     const {mode}=useTheme();
+
+    const handleDelete=async (id)=>{
+        console.log(id)
+        
+        const ref=doc(db,"bloglar",id);
+
+        await deleteDoc(ref);
+
+    }
 
     if (bloglar.length === 0) {
         return <div className="error">Aranan yazı bulunamadı</div>
@@ -17,6 +29,8 @@ export default function BlogList({bloglar}) {
                 <p>{blog.okunmaSuresi}</p>
                 <div>{blog.icerik.substring(0, 100)}...</div>
                 <Link to={`/blog/${blog.id}`}>Daha Fazla Oku</Link>
+                <img className='delete' onClick={()=>handleDelete(blog.id)}
+                src={DeleteIcon} alt="Yazı sil" />
                 </div>
             ))}
             
